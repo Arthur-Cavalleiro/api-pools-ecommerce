389cda9938cafa435acf2e44efc19697da428b3c
Arthur Cavalleiro <98990206+Arthur-Cavalleiro@users.noreply.github.com>
2023-11-29 00:44:35 -0300

    Update db.py

--- db.py
+++ db.py
@@ -2,5 +2,5 @@ import psycopg2 as ps
 
 
 def connect_db():
-    db = ps.connect(host='localhost', database='PI_piscinas', user='postgres', password='fabez848')
+    db = ps.connect(host='localhost', database='db_name', user='user_name', password='pgadmin4_password')
     return db
